8cd06d2ad7b6a7cf0c251dc4cbe9de79378c24e6
Craig Voisin <craigv@google.com>
2019-10-07 04:34:28 -0700

    Add Persona Broker service main and app.yaml
    PiperOrigin-RevId: 273262471
    Change-Id: I23bf3f51b4e294ddc79dce86676b99ba5e3a72de

---
+++ gcp/persona_broker/main.go
@@ -0,0 +1,48 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This package provides a persona broker service for offering a
+// playground environment where users can log in and manage the system
+// using personas. For configuration information see app.yaml.
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/GoogleCloudPlatform/healthcare-federated-access-services/lib/persona"
+	"github.com/GoogleCloudPlatform/healthcare-federated-access-services/lib/testkeys"
+)
+
+func main() {
+	key := &testkeys.PersonaBrokerKey
+	service := os.Getenv("DAM_SERVICE_NAME")
+	if len(service) == 0 {
+		service = "dam"
+	}
+	path := os.Getenv("CONFIG_PATH")
+	if len(path) == 0 {
+		path = "deploy/config"
+	}
+	oidcURL := os.Getenv("OIDC_URL")
+	if len(oidcURL) == 0 {
+		log.Fatalf("OIDC_URL must be provided")
+	}
+	port := os.Getenv("PORT")
+	broker, err := persona.NewBroker(oidcURL, key, service, path)
+	if err != nil {
+		log.Fatalf("starting broker: %v", err)
+	}
+	broker.Serve(port)
+}
