43d5b18480b64ea970238f135f4c6cf0194ef609
Kaveh Ghasemloo <ghasemloo@google.com>
2019-11-21 11:26:34 -0800

    ran the gen proto script
    PiperOrigin-RevId: 281794461
    Change-Id: I778855bac03efc1332b080a4e8b4033be6c70e14

--- proto/tokens/v1/tokens.pb.go
+++ proto/tokens/v1/tokens.pb.go
@@ -16,12 +16,17 @@
 // source: proto/tokens/v1/tokens.proto
 
 // Package v1 tokens provides protocol buffer versions of tokens API.
+
 package v1
 
 import (
+	context "context"
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
-	_ "github.com/golang/protobuf/ptypes/empty"
+	empty "github.com/golang/protobuf/ptypes/empty"
+	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 	math "math"
 )
 
@@ -431,4 +436,162 @@ var fileDescriptor_522bedb22d9068f2 = []byte{
 	0x9f, 0x23, 0x4d, 0xc4, 0x3c, 0xa4, 0x05, 0x8e, 0x66, 0x18, 0x61, 0x41, 0x05, 0x46, 0x23, 0x1a,
 	0x86, 0xc8, 0xf9, 0x88, 0x63, 0xb1, 0x64, 0x21, 0x72, 0x7f, 0xe7, 0x8f, 0x30, 0x35, 0x25, 0xf0,
 	0xea, 0x57, 0x00, 0x00, 0x00, 0xff, 0xff, 0xc1, 0x8c, 0x6e, 0xe8, 0x2b, 0x04, 0x00, 0x00,
+}
+
+// Reference imports to suppress errors if they are not otherwise used.
+var _ context.Context
+var _ grpc.ClientConn
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the grpc package it is being compiled against.
+const _ = grpc.SupportPackageIsVersion4
+
+// TokensClient is the client API for Tokens service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+type TokensClient interface {
+	// Gets the information for the specified token.
+	GetToken(ctx context.Context, in *GetTokenRequest, opts ...grpc.CallOption) (*Token, error)
+	// Deletes the specified token.
+	DeleteToken(ctx context.Context, in *DeleteTokenRequest, opts ...grpc.CallOption) (*empty.Empty, error)
+	// Lists the tokens.
+	ListTokens(ctx context.Context, in *ListTokensRequest, opts ...grpc.CallOption) (*ListTokensResponse, error)
+}
+
+type tokensClient struct {
+	cc *grpc.ClientConn
+}
+
+func NewTokensClient(cc *grpc.ClientConn) TokensClient {
+	return &tokensClient{cc}
+}
+
+func (c *tokensClient) GetToken(ctx context.Context, in *GetTokenRequest, opts ...grpc.CallOption) (*Token, error) {
+	out := new(Token)
+	err := c.cc.Invoke(ctx, "/tokens.v1.Tokens/GetToken", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *tokensClient) DeleteToken(ctx context.Context, in *DeleteTokenRequest, opts ...grpc.CallOption) (*empty.Empty, error) {
+	out := new(empty.Empty)
+	err := c.cc.Invoke(ctx, "/tokens.v1.Tokens/DeleteToken", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *tokensClient) ListTokens(ctx context.Context, in *ListTokensRequest, opts ...grpc.CallOption) (*ListTokensResponse, error) {
+	out := new(ListTokensResponse)
+	err := c.cc.Invoke(ctx, "/tokens.v1.Tokens/ListTokens", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// TokensServer is the server API for Tokens service.
+type TokensServer interface {
+	// Gets the information for the specified token.
+	GetToken(context.Context, *GetTokenRequest) (*Token, error)
+	// Deletes the specified token.
+	DeleteToken(context.Context, *DeleteTokenRequest) (*empty.Empty, error)
+	// Lists the tokens.
+	ListTokens(context.Context, *ListTokensRequest) (*ListTokensResponse, error)
+}
+
+// UnimplementedTokensServer can be embedded to have forward compatible implementations.
+type UnimplementedTokensServer struct {
+}
+
+func (*UnimplementedTokensServer) GetToken(ctx context.Context, req *GetTokenRequest) (*Token, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetToken not implemented")
+}
+func (*UnimplementedTokensServer) DeleteToken(ctx context.Context, req *DeleteTokenRequest) (*empty.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DeleteToken not implemented")
+}
+func (*UnimplementedTokensServer) ListTokens(ctx context.Context, req *ListTokensRequest) (*ListTokensResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListTokens not implemented")
+}
+
+func RegisterTokensServer(s *grpc.Server, srv TokensServer) {
+	s.RegisterService(&_Tokens_serviceDesc, srv)
+}
+
+func _Tokens_GetToken_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetTokenRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(TokensServer).GetToken(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/tokens.v1.Tokens/GetToken",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(TokensServer).GetToken(ctx, req.(*GetTokenRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Tokens_DeleteToken_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DeleteTokenRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(TokensServer).DeleteToken(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/tokens.v1.Tokens/DeleteToken",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(TokensServer).DeleteToken(ctx, req.(*DeleteTokenRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Tokens_ListTokens_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ListTokensRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(TokensServer).ListTokens(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/tokens.v1.Tokens/ListTokens",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(TokensServer).ListTokens(ctx, req.(*ListTokensRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _Tokens_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "tokens.v1.Tokens",
+	HandlerType: (*TokensServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "GetToken",
+			Handler:    _Tokens_GetToken_Handler,
+		},
+		{
+			MethodName: "DeleteToken",
+			Handler:    _Tokens_DeleteToken_Handler,
+		},
+		{
+			MethodName: "ListTokens",
+			Handler:    _Tokens_ListTokens_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "proto/tokens/v1/tokens.proto",
 }
