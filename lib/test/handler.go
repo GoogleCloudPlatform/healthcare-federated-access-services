c6a440ac3bb74e5865c8c3672b8f03e9817c38c9
Jianpeng Chao <chaopeng@google.com>
2020-02-19 10:25:30 -0800

    Use sub tests for HandlerTests
    For better output.
    
    PiperOrigin-RevId: 295999390
    Change-Id: I0054c31dd1084b5b9ea5ab9f229aee3eb17eec90

--- lib/test/handler.go
+++ lib/test/handler.go
@@ -26,6 +26,7 @@ import (
 
 	"github.com/google/go-cmp/cmp" /* copybara-comment */
 	"github.com/GoogleCloudPlatform/healthcare-federated-access-services/lib/persona" /* copybara-comment: persona */
+
 	cpb "github.com/GoogleCloudPlatform/healthcare-federated-access-services/proto/common/v1" /* copybara-comment: go_proto */
 	dampb "github.com/GoogleCloudPlatform/healthcare-federated-access-services/proto/dam/v1" /* copybara-comment: go_proto */
 )
@@ -70,91 +71,93 @@ func HandlerTests(t *testing.T, h serviceHandler, tests []HandlerTest, issuerURL
 
 	testOutput := make(map[string]string)
 	for _, test := range tests {
-		name := test.Name
-		if len(name) == 0 {
-			name = test.Method + " " + test.Path
-		}
-		pname := "dr_joe_elixir"
-		if len(test.Persona) > 0 {
-			pname = test.Persona
-		}
-		var p *cpb.TestPersona
-		if cfg != nil {
-			p = cfg.TestPersonas[pname]
-		}
-		acTok, _, err := persona.NewAccessToken(pname, issuerURL, TestClientID, test.Scope, p)
-		if err != nil {
-			t.Fatalf("persona.NewAccessToken(%q, %q, _, _) failed: %v", pname, issuerURL, err)
-		}
-		target := fmt.Sprintf("%s?client_id=%s&client_secret=%s", test.Path, TestClientID, TestClientSecret)
-		if len(test.Params) > 0 {
-			target += "&" + test.Params
-		}
-		var Input io.Reader
-		varInput := false
-		InputStr := test.Input
-		if len(test.Input) > 0 {
-			for match := varRE.FindStringSubmatch(InputStr); match != nil; match = varRE.FindStringSubmatch(InputStr) {
-				varInput = true
-				ref := match[2]
-				if len(testOutput[ref]) == 0 {
-					t.Fatalf("test %q uses %q, but %q must be tested separately earlier in the test list", name, ref, ref)
+		t.Run(test.Name, func(t *testing.T) {
+			name := test.Name
+			if len(name) == 0 {
+				name = test.Method + " " + test.Path
+			}
+			pname := "dr_joe_elixir"
+			if len(test.Persona) > 0 {
+				pname = test.Persona
+			}
+			var p *cpb.TestPersona
+			if cfg != nil {
+				p = cfg.TestPersonas[pname]
+			}
+			acTok, _, err := persona.NewAccessToken(pname, issuerURL, TestClientID, test.Scope, p)
+			if err != nil {
+				t.Fatalf("persona.NewAccessToken(%q, %q, _, _) failed: %v", pname, issuerURL, err)
+			}
+			target := fmt.Sprintf("%s?client_id=%s&client_secret=%s", test.Path, TestClientID, TestClientSecret)
+			if len(test.Params) > 0 {
+				target += "&" + test.Params
+			}
+			var Input io.Reader
+			varInput := false
+			InputStr := test.Input
+			if len(test.Input) > 0 {
+				for match := varRE.FindStringSubmatch(InputStr); match != nil; match = varRE.FindStringSubmatch(InputStr) {
+					varInput = true
+					ref := match[2]
+					if len(testOutput[ref]) == 0 {
+						t.Fatalf("test %q uses %q, but %q must be tested separately earlier in the test list", name, ref, ref)
+					}
+					InputStr = strings.Replace(InputStr, match[1], testOutput[ref], -1)
 				}
-				InputStr = strings.Replace(InputStr, match[1], testOutput[ref], -1)
+				Input = strings.NewReader(InputStr)
 			}
-			Input = strings.NewReader(InputStr)
-		}
-		r := httptest.NewRequest(test.Method, target, Input)
-		if test.IsForm {
-			r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-		}
-		r.Header.Set("Authorization", "Bearer "+string(acTok))
-		if len(test.LinkPersona) > 0 {
-			linkTok, _, err := persona.NewAccessToken(test.LinkPersona, issuerURL, TestClientID, test.LinkScope, nil)
-			if err != nil {
-				t.Fatalf("persona.NewAccessToken(%q, %q, _, _) failed: %v", test.LinkPersona, issuerURL, err)
+			r := httptest.NewRequest(test.Method, target, Input)
+			if test.IsForm {
+				r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 			}
-			r.Header.Set("X-Link-Authorization", "Bearer "+string(linkTok))
-		}
-		w := httptest.NewRecorder()
-		h.ServeHTTP(w, r)
-		Output := w.Body.String()
-		testOutput[name] = Output
-		hasError := false
+			r.Header.Set("Authorization", "Bearer "+string(acTok))
+			if len(test.LinkPersona) > 0 {
+				linkTok, _, err := persona.NewAccessToken(test.LinkPersona, issuerURL, TestClientID, test.LinkScope, nil)
+				if err != nil {
+					t.Fatalf("persona.NewAccessToken(%q, %q, _, _) failed: %v", test.LinkPersona, issuerURL, err)
+				}
+				r.Header.Set("X-Link-Authorization", "Bearer "+string(linkTok))
+			}
+			w := httptest.NewRecorder()
+			h.ServeHTTP(w, r)
+			Output := w.Body.String()
+			testOutput[name] = Output
+			hasError := false
 
-		if w.Code != test.Status {
-			hasError = true
-			t.Errorf("test %q returned wrong status code: got %d want %d", name, w.Code, test.Status)
-		}
-		switch {
-		case strings.HasPrefix(test.Output, "^"):
-			re, err := regexp.Compile(test.Output)
-			if err != nil {
-				t.Fatalf("test %q cannot compile regexp Output %q: %v", name, test.Output, err)
-			}
-			if !re.Match([]byte(Output)) {
+			if w.Code != test.Status {
 				hasError = true
-				t.Errorf("test %q returned unexpected body, got:\n%s\n\nwant regexp match of:\n%s", name, Output, test.Output)
+				t.Errorf("test %q returned wrong status code: got %d want %d", name, w.Code, test.Status)
 			}
-		case strings.HasPrefix(test.Output, "*") && strings.HasSuffix(test.Output, "*"):
-			q := regexp.QuoteMeta(test.Output[1 : len(test.Output)-1])
-			q = strings.Replace(q, "\\*", ".*", -1)
-			re, err := regexp.Compile(q)
-			if err != nil {
-				t.Fatalf("test %q cannot compile regexp Output %q with regex %q: %v", name, test.Output, q, err)
+			switch {
+			case strings.HasPrefix(test.Output, "^"):
+				re, err := regexp.Compile(test.Output)
+				if err != nil {
+					t.Fatalf("test %q cannot compile regexp Output %q: %v", name, test.Output, err)
+				}
+				if !re.Match([]byte(Output)) {
+					hasError = true
+					t.Errorf("test %q returned unexpected body, got:\n%s\n\nwant regexp match of:\n%s", name, Output, test.Output)
+				}
+			case strings.HasPrefix(test.Output, "*") && strings.HasSuffix(test.Output, "*"):
+				q := regexp.QuoteMeta(test.Output[1 : len(test.Output)-1])
+				q = strings.Replace(q, "\\*", ".*", -1)
+				re, err := regexp.Compile(q)
+				if err != nil {
+					t.Fatalf("test %q cannot compile regexp Output %q with regex %q: %v", name, test.Output, q, err)
+				}
+				if !re.Match([]byte(Output)) {
+					hasError = true
+					t.Errorf("test %q returned unexpected body, got:\n%s\n\nwant substring match of:\n%s\n\nregex:\n%s", name, Output, test.Output, q)
+				}
+			default:
+				if diff := cmp.Diff(test.Output, Output, test.CmpOptions); diff != "" {
+					hasError = true
+					t.Errorf("test %q returned mismatching body (-want +got):\n%s", name, diff)
+				}
 			}
-			if !re.Match([]byte(Output)) {
-				hasError = true
-				t.Errorf("test %q returned unexpected body, got:\n%s\n\nwant substring match of:\n%s\n\nregex:\n%s", name, Output, test.Output, q)
+			if hasError && varInput {
+				t.Logf("test %q Input value was: %s", name, InputStr)
 			}
-		default:
-			if diff := cmp.Diff(test.Output, Output, test.CmpOptions); diff != "" {
-				hasError = true
-				t.Errorf("test %q returned mismatching body (-want +got):\n%s", name, diff)
-			}
-		}
-		if hasError && varInput {
-			t.Logf("test %q Input value was: %s", name, InputStr)
-		}
+		})
 	}
 }
