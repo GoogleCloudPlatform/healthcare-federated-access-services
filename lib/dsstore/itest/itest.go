5db5c9ef002db05f903d65d73e7a8abe54cc36ce
Kaveh Ghasemloo <ghasemloo@google.com>
2020-02-28 08:01:30 -0800

    add more tests for storage transactions
    PiperOrigin-RevId: 297844170
    Change-Id: I4017900b745e79bd9297e9972e53080d4ea15abd

--- lib/dsstore/itest/itest.go
+++ lib/dsstore/itest/itest.go
@@ -18,6 +18,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 
 	glog "github.com/golang/glog" /* copybara-comment */
 	"cloud.google.com/go/datastore" /* copybara-comment: datastore */
@@ -33,6 +34,9 @@ var (
 	fakeProjectID   = flag.String("project_id", "fake-project-id", "")
 	fakeServiceName = "fake-service-name"
 	fakeConfigPath  = "fake-config-path"
+	fakeDataType    = "fake-datatype"
+	fakeRealm       = "fake-realm"
+	fakeUser        = "fake-user"
 )
 
 func main() {
@@ -44,6 +48,17 @@ func main() {
 		glog.Exitf("datastore.NewClient(...) failed: %v", err)
 	}
 
+	scenarioSimple(ctx, c)
+	scenarioTransactionsConflictingLinearizable(ctx, c)
+	scenarioTransactionsConflictingNonLinearizable(ctx, c)
+	scenarioTransactionsReadAfterWrite(ctx, c)
+
+	fmt.Println("All tests passed.")
+}
+
+func scenarioSimple(ctx context.Context, c *datastore.Client) {
+	// Scenario: write, read-check, read-modify-write, read-check, delete, read-check
+	id := "fake-id-simple"
 	s := dsstore.New(c, *fakeProjectID, fakeServiceName, fakeConfigPath)
 
 	{
@@ -53,7 +68,7 @@ func main() {
 			glog.Exitf("store.Tx(true) failed: %v", err)
 		}
 		entity := &dpb.Duration{Seconds: 60}
-		if err := s.WriteTx("fake-datatype", "fake-realm", "fake-user", "fake-id", storage.LatestRev, entity, nil, tx); err != nil {
+		if err := s.WriteTx(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, entity, nil, tx); err != nil {
 			glog.Exitf("store.WriteTx(...) failed: %v", err)
 		}
 		if err := tx.Finish(); err != nil {
@@ -64,7 +79,7 @@ func main() {
 	{
 		// Read to check it is created.
 		got := &dpb.Duration{}
-		if err := s.Read("fake-datatype", "fake-realm", "fake-user", "fake-id", storage.LatestRev, got); err != nil {
+		if err := s.Read(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, got); err != nil {
 			glog.Exitf("store.Read(...) failed: %v", err)
 		}
 		want := &dpb.Duration{Seconds: 60}
@@ -80,13 +95,13 @@ func main() {
 			glog.Exitf("store.Tx(true) failed: %v", err)
 		}
 		resp := &dpb.Duration{}
-		if err := s.ReadTx("fake-datatype", "fake-realm", "fake-user", "fake-id", storage.LatestRev, resp, tx); err != nil {
+		if err := s.ReadTx(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, resp, tx); err != nil {
 			glog.Exitf("store.ReadTx(...) failed: %v", err)
 		}
 
 		resp.Seconds = resp.Seconds + 60
 
-		if err := s.WriteTx("fake-datatype", "fake-realm", "fake-user", "fake-id", storage.LatestRev, resp, nil, tx); err != nil {
+		if err := s.WriteTx(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, resp, nil, tx); err != nil {
 			glog.Exitf("store.WriteTx(...) failed: %v", err)
 		}
 		if err := tx.Finish(); err != nil {
@@ -97,7 +112,7 @@ func main() {
 	{
 		// Read to check it is updated.
 		got := &dpb.Duration{}
-		if err := s.Read("fake-datatype", "fake-realm", "fake-user", "fake-id", storage.LatestRev, got); err != nil {
+		if err := s.Read(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, got); err != nil {
 			glog.Exitf("store.Read(...) failed: %v", err)
 		}
 		want := &dpb.Duration{Seconds: 120}
@@ -112,7 +127,7 @@ func main() {
 		if err != nil {
 			glog.Exitf("store.Tx(true) failed: %v", err)
 		}
-		if err := s.DeleteTx("fake-datatype", "fake-realm", "fake-user", "fake-id", storage.LatestRev, tx); err != nil {
+		if err := s.DeleteTx(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, tx); err != nil {
 			glog.Exitf("store.DeleteTx(...) failed: %v", err)
 		}
 		if err := tx.Finish(); err != nil {
@@ -122,9 +137,138 @@ func main() {
 
 	{
 		// Read to check it is deleted.
-		if err := s.Read("fake-datatype", "fake-realm", "fake-user", "fake-id", storage.LatestRev, &dpb.Duration{}); status.Code(err) != codes.NotFound {
+		if err := s.Read(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, &dpb.Duration{}); status.Code(err) != codes.NotFound {
 			glog.Exitf("store.Read(...) = %v, want error with code %v", err, codes.NotFound)
 		}
 	}
+}
 
+func scenarioTransactionsConflictingLinearizable(ctx context.Context, c *datastore.Client) {
+	// Scenario: two concurrent write transactions, the second to commit prevails.
+	id := "fake-id-TransactionsConflictingLinearizable"
+	s := dsstore.New(c, *fakeProjectID, fakeServiceName, fakeConfigPath)
+
+	tx1, err := s.Tx(true)
+	if err != nil {
+		glog.Exitf("store.Tx(true) failed: %v", err)
+	}
+	tx2, err := s.Tx(true)
+	if err != nil {
+		glog.Exitf("store.Tx(true) failed: %v", err)
+	}
+
+	if err := s.WriteTx(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, &dpb.Duration{Seconds: 1}, nil, tx1); err != nil {
+		glog.Exitf("store.WriteTx(...) failed: %v", err)
+	}
+
+	if err := s.WriteTx(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, &dpb.Duration{Seconds: 2}, nil, tx2); err != nil {
+		glog.Exitf("store.WriteTx(...) failed: %v", err)
+	}
+
+	if err := tx1.Finish(); err != nil {
+		glog.Exitf("tx.Finish() failed: %v", err)
+	}
+	if err := tx2.Finish(); err != nil {
+		glog.Exitf("tx.Finish() failed: %v", err)
+	}
+
+	got := &dpb.Duration{}
+	if err := s.Read(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, got); err != nil {
+		glog.Exitf("store.Read(...) failed: %v", err)
+	}
+	want := &dpb.Duration{Seconds: 2}
+	if got.GetSeconds() != want.GetSeconds() {
+		glog.Exitf("store.Read(...) = %v, want %v", got, want)
+	}
+}
+
+func scenarioTransactionsConflictingNonLinearizable(ctx context.Context, c *datastore.Client) {
+	// Scenario: two concurrent RMW transactions, the second to commit fails.
+	id := "fake-id-TransactionsConflictingNonLinearizable"
+	s := dsstore.New(c, *fakeProjectID, fakeServiceName, fakeConfigPath)
+
+	tx1, err := s.Tx(true)
+	if err != nil {
+		glog.Exitf("store.Tx(true) failed: %v", err)
+	}
+	tx2, err := s.Tx(true)
+	if err != nil {
+		glog.Exitf("store.Tx(true) failed: %v", err)
+	}
+
+	// Read so the transactions cannot be linearized.
+	s.ReadTx(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, &dpb.Duration{}, tx1)
+	if err := s.WriteTx(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, &dpb.Duration{Seconds: 1}, nil, tx1); err != nil {
+		glog.Exitf("store.WriteTx(...) failed: %v", err)
+	}
+
+	// Read so the transactions cannot be linearized.
+	s.ReadTx(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, &dpb.Duration{}, tx2)
+	if err := s.WriteTx(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, &dpb.Duration{Seconds: 2}, nil, tx2); err != nil {
+		glog.Exitf("store.WriteTx(...) failed: %v", err)
+	}
+
+	if err := tx1.Finish(); err != nil {
+		glog.Exitf("tx.Finish() failed: %v", err)
+	}
+	if err := tx2.Finish(); err != datastore.ErrConcurrentTransaction {
+		glog.Exitf("tx.Finish() failed: %v", err)
+	}
+
+	got := &dpb.Duration{}
+	if err := s.Read(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, got); err != nil {
+		glog.Exitf("store.Read(...) failed: %v", err)
+	}
+	want := &dpb.Duration{Seconds: 1}
+	if got.GetSeconds() != want.GetSeconds() {
+		glog.Exitf("store.Read(...) = %v, want %v", got, want)
+	}
+}
+
+func scenarioTransactionsReadAfterWrite(ctx context.Context, c *datastore.Client) {
+	// Scenario: one transaction, write followed by read, read doesn't see the write.
+	id := "fake-id-TransactionsReadAfterWrite"
+	s := dsstore.New(c, *fakeProjectID, fakeServiceName, fakeConfigPath)
+
+	{
+		if err := s.Write(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, &dpb.Duration{Seconds: 60}, nil); err != nil {
+			glog.Exitf("store.WriteTx(...) failed: %v", err)
+		}
+	}
+
+	{
+		tx, err := s.Tx(true)
+		if err != nil {
+			glog.Exitf("store.Tx(true) failed: %v", err)
+		}
+
+		e := &dpb.Duration{Seconds: 120}
+		if err := s.WriteTx(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, e, nil, tx); err != nil {
+			glog.Exitf("store.WriteTx(...) failed: %v", err)
+		}
+
+		got := &dpb.Duration{}
+		if err := s.ReadTx(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, got, tx); err != nil {
+			glog.Exitf("store.ReadTx(...) failed: %v", err)
+		}
+		want := &dpb.Duration{Seconds: 60}
+		if got.GetSeconds() != want.GetSeconds() {
+			glog.Exitf("store.ReadTx(...) = %v, want %v", got, want)
+		}
+
+		if err := tx.Finish(); err != nil {
+			glog.Exitf("tx.Finish() failed: %v", err)
+		}
+	}
+
+	{
+		got := &dpb.Duration{}
+		if err := s.Read(fakeDataType, fakeRealm, fakeUser, id, storage.LatestRev, got); err != nil {
+			glog.Exitf("store.Read(...) failed: %v", err)
+		}
+		want := &dpb.Duration{Seconds: 120}
+		if got.GetSeconds() != want.GetSeconds() {
+			glog.Exitf("store.Read(...) = %v, want %v", got, want)
+		}
+	}
 }
