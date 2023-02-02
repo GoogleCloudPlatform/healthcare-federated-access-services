fafdf1fc4d2beafba791c8e264546ecd00d6f423
Google Cloud Healthcare Team <noreply@google.com>
2023-02-02 09:04:20 -0800

    Internal change
    PiperOrigin-RevId: 506636959
    Change-Id: Ib9c964a2fe5acbdb343a5b225ad31842cde571d0

--- lib/aws/warehouse.go
+++ lib/aws/warehouse.go
@@ -24,10 +24,10 @@ import (
 	"strings"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws" /* copybara-comment */
-	"github.com/aws/aws-sdk-go/aws/awserr" /* copybara-comment */
-	"github.com/aws/aws-sdk-go/service/iam" /* copybara-comment */
-	"github.com/aws/aws-sdk-go/service/sts" /* copybara-comment */
+	"google3/third_party/golang/github_com/aws/aws_sdk_go/v/v1/aws/aws"
+	"google3/third_party/golang/github_com/aws/aws_sdk_go/v/v1/aws/awserr/awserr"
+	"google3/third_party/golang/github_com/aws/aws_sdk_go/v/v1/service/iam/iam"
+	"google3/third_party/golang/github_com/aws/aws_sdk_go/v/v1/service/sts/sts"
 	"github.com/cenkalti/backoff" /* copybara-comment */
 	"bitbucket.org/creachadair/stringset" /* copybara-comment */
 	"github.com/pborman/uuid" /* copybara-comment */
